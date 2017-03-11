9bf9bad6633ec62ec33a6b11d0ce1dbc16e46bb6
ellebayless <elle@ellebayles.com>
2017-03-10 21:51:37 -0700

    Added random chosen album functionality. Functionality complete.

--- app.js
+++ app.js
@@ -5,19 +5,14 @@ $(document).ready(function() {
 
     $.getJSON('https://lit-fortress-6467.herokuapp.com/object', function(data) {
 
-      for (var i = 0; i < 3; i++) {
+      for (var i = 0; i < data.results.length; i++) {
+        var randomChooser = Math.floor(Math.random() * (data.results.length - 0)) + 0;
+        var j = randomChooser;
         var cover = document.createElement('img');
-        cover.setAttribute("src", "images/" + data.results[i].cover_art);
+        cover.setAttribute("src", "images/" + data.results[j].cover_art);
         var body = $(".cover1");
         cover.style.margin = '20px';
         body.append(cover);
       }
     });
 });
-
-// function coverChooser(min, max) {
-//   min = Math.ceil(0);
-//   max = Math.floor(data.results.length);
-//   var chosenOne = Math.floor(Math.random() * (max - min)) + min;
-//   return chosenOne;
-// }
